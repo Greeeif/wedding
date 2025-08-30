e765760ad5a7ee1a9f0f79fe4d6a17802a2f7c2d
Greeeif <161254377+Greeeif@users.noreply.github.com>
2025-08-30 11:28:02 +0100

    corrected next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig =  
+  {
+  "extends": ["next/core-web-vitals"],
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-head-element": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
+} 
+;
 
 export default nextConfig;
