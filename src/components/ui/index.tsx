aae2ee84a51e2f4920be5cb26a6c232d965b9a77
greeif <griffiths.alex@gmail.com>
2025-09-23 21:21:29 +0200

    test changes

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -2,15 +2,14 @@
 export { InvitationCard } from './InvitationCard';
 export { BotanicalBackground } from './BotanicalBackground';
 export { Monogram } from './Monogram';
-export { SectionHeader } from './SectionHeader';
 export { BlissCard } from './BlissCard';
 export { BlissButton } from './BlissButton';
-export { BlissInput } from './BlissInput'
 export { BlissTextarea } from './BlissTextarea';
 export { ImagePlaceholder } from './ImagePlaceholder';
 export { DetailCard } from './DetailCard';
 export { EditorialGrid } from './EditorialGrid';
 export { BlissNavigation } from './BlissNavigation';
+export { SectionHeader } from './SectionHeader';    
 
 // Legacy exports for compatibility
 export { Button } from './Button';
