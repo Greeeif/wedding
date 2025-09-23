b4b83b0ca01f5b6f6370f7ab246add2169cc3a53
Greeeif <161254377+Greeeif@users.noreply.github.com>
2025-09-23 20:48:15 +0100

    adjustments made to file names and build packages

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig =  
   {
-  "extends": ["next/core-web-vitals"],
-  "rules": {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-head-element": "off",
-    "@next/next/no-page-custom-font": "off"
-  }
+
 } 
 ;
 
