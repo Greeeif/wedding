9f9e826c187c036967ba01cdc68533cbae53cb84
Greeeif <161254377+Greeeif@users.noreply.github.com>
2025-11-15 21:11:21 +0000

    good working start for login page to main page transition

--- src/components/ui/PageTransition.tsx
+++ src/components/ui/PageTransition.tsx
@@ -14,8 +14,8 @@ export const PageTransition = ({ children }: PageTransitionProps) => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{
-        duration: 0.5,
-        ease: [0.25, 0.1, 0.25, 1], // Custom easing for elegance
+        duration: 7,
+        ease: [0.25, 0.25, 0.1, 1], // Custom easing for elegance
       }}
     >
       {children}
